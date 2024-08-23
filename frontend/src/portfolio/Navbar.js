49bc3f0a3b7a4f14642e57875bdb15cb09ba2003
nahilord124578 <nahom.mitiku@aait.edu.et>
2024-08-23 14:54:46 +0300

    klds

--- frontend/src/portfolio/Navbar.js
+++ frontend/src/portfolio/Navbar.js
@@ -9,26 +9,24 @@ const Navbar = () => {
                     My Portfolio
                 </div>
                 <ul className="flex space-x-4">
-                    <li>
+                    
                         <Link to="/" className="text-white hover:text-gray-400">
                             Home
                         </Link>
-                    </li>
-                    <li>
+                    
+                    
                         <Link to="/resume" className="text-white hover:text-gray-400">
                             Resume
                         </Link>
-                    </li>
-                    <li>
+                    
                         <Link to="/projects" className="text-white hover:text-gray-400">
                             Projects
                         </Link>
-                    </li>
-                    <li>
+                    
                         <Link to="/contact" className="text-white hover:text-gray-400">
                             Contact
                         </Link>
-                    </li>
+                   
                 </ul>
             </div>
         </nav>
