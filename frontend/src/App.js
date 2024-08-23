5ae3b16b6bda0993fd98b9264ee9eef92704c715
nahilord124578 <nahom.mitiku@aait.edu.et>
2024-08-23 14:52:17 +0300

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./portfolio/Home";
 import Navbar from "./portfolio/Navbar";
 import Resume from "./portfolio/Resume";
 import Projects from "./portfolio/Projects";
-import Contact from "./portfolio/Contact"; // Assuming you have a Contact component
+import Contact from "./portfolio/Contact";
 
 function App() {
     return (
-        <Router>
+        <div>
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
@@ -16,7 +16,7 @@ function App() {
                 <Route path="/projects" element={<Projects />} />
                 <Route path="/contact" element={<Contact />} />
             </Routes>
-        </Router>
+        </div>
     );
 }
 
--- frontend/src/portfolio/Navbar.js
+++ frontend/src/portfolio/Navbar.js
@@ -3,29 +3,29 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="bg-blue-600 p-4">
+        <nav className="bg-gray-800 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-white text-2xl font-bold">
                     My Portfolio
                 </div>
                 <ul className="flex space-x-4">
                     <li>
-                        <Link to="/" className="text-white hover:text-blue-300">
+                        <Link to="/" className="text-white hover:text-gray-400">
                             Home
                         </Link>
                     </li>
                     <li>
-                        <Link to="/resume" className="text-white hover:text-blue-300">
+                        <Link to="/resume" className="text-white hover:text-gray-400">
                             Resume
                         </Link>
                     </li>
                     <li>
-                        <Link to="/projects" className="text-white hover:text-blue-300">
+                        <Link to="/projects" className="text-white hover:text-gray-400">
                             Projects
                         </Link>
                     </li>
                     <li>
-                        <Link to="/contact" className="text-white hover:text-blue-300">
+                        <Link to="/contact" className="text-white hover:text-gray-400">
                             Contact
                         </Link>
                     </li>
