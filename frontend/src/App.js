70a9dab5916e8f521c6874dc2721839a51a859a8
nahilord124578 <nahom.mitiku@aait.edu.et>
2024-08-23 14:35:27 +0300

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,20 @@ import Home from "./portfolio/Home";
 import Navbar from "./portfolio/Navbar";
 import Resume from "./portfolio/Resume";
 import Projects from "./portfolio/Projects";
+import Contact from "./portfolio/Contact"; // Assuming you have a Contact component
+
 function App() {
     return (
-      <div>
-<Navbar />
-<Home />
-<Resume />
-<Projects />
-      </div>
-  );
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/resume" element={<Resume />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
