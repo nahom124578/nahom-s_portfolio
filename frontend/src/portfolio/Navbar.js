5bf51b01e4964102fdff492d0f9c584f05128c8c
nahilord124578 <nahom.mitiku@aait.edu.et>
2024-08-23 14:19:09 +0300

    navbar

--- frontend/src/portfolio/Navbar.js
+++ frontend/src/portfolio/Navbar.js
@@ -1,18 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
-        <div>
-            <ul>
-                
-                Home
-                Resume 
-                Projects
-                Contact
-            </ul>
-         
-           
-        </div>
-    )
-}
+        <nav className="bg-blue-600 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-white text-2xl font-bold">
+                    My Portfolio
+                </div>
+                <ul className="flex space-x-4">
+                    <li>
+                        <Link
+                            to="/"
+                            className="text-white hover:text-blue-300 transition duration-300"
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/resume"
+                            className="text-white hover:text-blue-300 transition duration-300"
+                        >
+                            Resume
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/projects"
+                            className="text-white hover:text-blue-300 transition duration-300"
+                        >
+                            Projects
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/contact"
+                            className="text-white hover:text-blue-300 transition duration-300"
+                        >
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
 export default Navbar;
