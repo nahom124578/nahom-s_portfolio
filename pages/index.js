9968ce9cc66e7746f71f7269734aab9e5874acdc
nahilord124578 <nahom.mitiku@aait.edu.et>
2024-09-24 16:34:34 +0300

    de

--- pages/index.js
+++ pages/index.js
@@ -43,12 +43,11 @@ export default function Home() {
                 />
               </li>
               <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="/resume"
-                >
-                  Resume
-                </a>
+                <Link href="/resume">
+                  <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8">
+                    Resume
+                  </a>
+                </Link>
               </li>
             </ul>
           </nav>
@@ -68,15 +67,15 @@ export default function Home() {
               <AiFillYoutube />
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" />
+              <Image src={deved} layout="fill" objectFit="cover" alt="Developer Image" />
             </div>
           </div>
         </section>
         <section>
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2">frontend</h3>
+              <Image src={design} width={100} height={100} alt="Design Icon" />
+              <h3 className="text-lg font-medium pt-8 pb-2">Frontend</h3>
               <p className="py-2">Creating elegant web pages.</p>
               <h4 className="py-4 text-teal-600">Tools I Use</h4>
               <p className="text-gray-800 py-1">Figma</p>
@@ -85,11 +84,9 @@ export default function Home() {
               <p className="text-gray-800 py-1">Next.js</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} />
+              <Image src={code} width={100} height={100} alt="Code Icon" />
               <h3 className="text-lg font-medium pt-8 pb-2">Backend & Database</h3>
-              <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a reality.
-              </p>
+              <p className="py-2">Do you have an idea for your next great website? Let's make it a reality.</p>
               <h4 className="py-4 text-teal-600">Tools I Use</h4>
               <p className="text-gray-800 py-1">Node.js</p>
               <p className="text-gray-800 py-1">GoLang</p>
@@ -97,16 +94,14 @@ export default function Home() {
               <p className="text-gray-800 py-1">PostgreSQL</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
+              <Image src={consulting} width={100} height={100} alt="Consulting Icon" />
               <h3 className="text-lg font-medium pt-8 pb-2">Hardware & Electrical</h3>
-              <p className="py-2">
-                Are you interested in real hardware projects? I can give you tips and tricks to level it up.
-              </p>
+              <p className="py-2">Are you interested in real hardware projects? I can give you tips and tricks to level it up.</p>
               <h4 className="py-4 text-teal-600">Tools I Use</h4>
               <p className="text-gray-800 py-1">Proteus</p>
-              <p className="text-gray-800 py-1">MATLAB</p>
+              <p className="text-gray-800 py-1">Matlab</p>
               <p className="text-gray-800 py-1">Packet Tracer</p>
-              <p className="text-gray-800 py-1">Many Electronic tools</p>
+              <p className="text-gray-800 py-1">Many Electronic Tools</p>
             </div>
           </div>
         </section>
@@ -114,7 +109,7 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white">Projects</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a developer and with other hardware projects, I have done different projects. 
+              Since the beginning of my journey as a developer and in other hardware projects, I have done different projects. Here are some of them.
             </p>
             <p>
               <span className="text-teal-500">Here are some of them:</span>
@@ -123,74 +118,74 @@ export default function Home() {
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             {/* Project 1 */}
             <div className="basis-1/3 flex-1">
-              <a href="https://github.com/nahom124578/mygit.git" target="_blank" rel="noopener noreferrer">
+              <Link href="https://github.com/nahom124578/mygit.git">
                 <Image
                   className="rounded-lg object-cover"
                   layout="responsive"
                   src={web1}
                   alt="Project 1"
                 />
-              </a>
+              </Link>
             </div>
 
             {/* Project 2 */}
             <div className="basis-1/3 flex-1">
-              <a href="https://github.com/DB-SQL-PROJECT/DB-SQL.git" target="_blank" rel="noopener noreferrer">
+              <Link href="https://github.com/DB-SQL-PROJECT/DB-SQL.git">
                 <Image
                   className="rounded-lg object-cover"
                   layout="responsive"
                   src={web2}
                   alt="Project 2"
                 />
-              </a>
+              </Link>
             </div>
 
             {/* Project 3 */}
             <div className="basis-1/3 flex-1">
-              <a href="https://github.com/nahom124578/packet-sniffing.git" target="_blank" rel="noopener noreferrer">
+              <Link href="https://github.com/nahom124578/packet-sniffing.git">
                 <Image
                   className="rounded-lg object-cover"
                   layout="responsive"
                   src={web3}
                   alt="Project 3"
                 />
-              </a>
+              </Link>
             </div>
 
             {/* Project 4 */}
             <div className="basis-1/3 flex-1">
-              <a href="https://github.com/yourusername/project4" target="_blank" rel="noopener noreferrer">
+              <Link href="https://github.com/yourusername/project4">
                 <Image
                   className="rounded-lg object-cover"
                   layout="responsive"
                   src={web4}
                   alt="Project 4"
                 />
-              </a>
+              </Link>
             </div>
 
             {/* Project 5 */}
             <div className="basis-1/3 flex-1">
-              <a href="https://github.com/ABHKNY-Summer-Projects/ECommerce_Website.git" target="_blank" rel="noopener noreferrer">
+              <Link href="https://github.com/ABHKNY-Summer-Projects/ECommerce_Website.git">
                 <Image
                   className="rounded-lg object-cover"
                   layout="responsive"
                   src={web5}
                   alt="Project 5"
                 />
-              </a>
+              </Link>
             </div>
 
             {/* Project 6 */}
             <div className="basis-1/3 flex-1">
-              <a href="https://github.com/yourusername/project6" target="_blank" rel="noopener noreferrer">
+              <Link href="https://github.com/nahom124578/MyPortfolio.git">
                 <Image
                   className="rounded-lg object-cover"
                   layout="responsive"
                   src={web6}
                   alt="Project 6"
                 />
-              </a>
+              </Link>
             </div>
           </div>
         </section>
